0900372de81553e40e143be2878f60d1f6b0da91
Vibhav Bobade <vibhav.bobde@gmail.com>
2019-07-28 08:05:36 +0530

    Updated setup.sh for usability

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
+# Tmux terminal init script
+sudo ln -s $(pwd)/tmuxscripts/settermvibhav /usr/local/bin/
+
+# zshrc setup
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 rm ~/.zshrc
+cp $(pwd)/dotfiles/.zshrc ~/.zshrc 
 
-ln -s ./dotfiles/.zshrc ~/.zshrc && sudo ln -s ./tmuxscripts/setterm /bin/setterm
+# JenkinsInstallPlugin 
+sudo ln -s $(pwd)/jenkinstools/jenkinsInstallPlugin/jip /usr/local/bin/
